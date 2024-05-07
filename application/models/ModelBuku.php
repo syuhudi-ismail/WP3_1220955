4387c1be10c9825e947b3cdd2801a1a304b6b2b2
syuhudi-ismail <sodiksup21@gmail/com>
2024-05-07 20:34:12 +0700

    pertemuan 6

--- application/models/ModelBuku.php
+++ application/models/ModelBuku.php
@@ -73,6 +73,9 @@ class ModelBuku extends CI_Model
         $this->db->where($where);
         return $this->db->get();
     }
-
-
+    public function getLimitBuku(){
+        $this->db->limit(5);
+        return $this->db->get('buku');
+      }
+  
 }
