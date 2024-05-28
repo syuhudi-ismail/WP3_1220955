642de94e60de9eadd43d598fb84c65d3b9f8eb54
syuhudi-ismail <sodiksup21@gmail.com>
2024-05-28 19:09:20 +0700

    pertemuan 9

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -29,4 +29,36 @@ class Laporan extends CI_Controller
 
     $this->load->view('buku/laporan_print_buku',$data);
     }
+    public function laporan_buku_pdf()
+    {
+      $data['buku']=$this->ModelBuku->getBuku()->result_array();
+       //script untuk dompdf php versi 5
+       $this->load->library('dompdf_gen');
+       // script untuk dompdf php versi 7.1.0 keatas
+       /*$sroot = $_SERVER['DOCUMENT_ROOT'];
+       include $sroot."/pustaka-booking/application/third_party/dompdf/autoload.inc.php";
+       $dompdf = new Dompdf\Dompdf();*/
+       $this->load->view('buku/laporan_pdf_buku', $data);
+       $paper_size = 'A4'; // ukuran kertas
+       $orientation = 'landscape'; //tipe format kertas potrait atau landscape
+       $html = $this->output->get_output();
+      //script untuk dompdf php versi 5
+       $this->dompdf->set_paper($paper_size, $orientation);
+       // Convert to PDF
+       $this->dompdf->load_html($html);
+       $this->dompdf->render();
+       $this->dompdf->stream("laporan_data_buku.pdf", array('Attachment' => 0));
+       // nama file pdf yang di hasilkan
+      // script untuk dompdf php versi 7.1.0 keatas
+       /*$dompdf->set_paper($paper_size, $orientation);
+       $dompdf->load_html($html);
+       $dompdf->render();
+       $dompdf->stream("laporan_data_buku.pdf", array('Attachment' => 0));*/
+    }
+    public function export_excel(){
+      $data=array(
+      'title'=>'Laporan Buku',
+      'buku'=>$this->ModelBuku->getBuku()->result_array());
+      $this->load->view('buku/export_excel_buku',$data);
+    }
 }
